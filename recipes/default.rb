a2e7e25a9bd6316753ac123baf06bc8246e0337b
lhasadreams <adrian.daniels@me.com>
2017-04-25 12:30:54 +0100

    Create cookbook and add mongodb to the apt repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+apt_repository 'mongodb' do
+  uri        'http://repo.mongodb.org/apt/ubuntu'
+  distribution 'trusty/mongodb-org/3.4'
+  components ['multiverse']
+  key '0C49F3730359A14518585931BC711F9BA15703C6'
+  keyserver 'keyserver.ubuntu.com'
+  action :add
+end
